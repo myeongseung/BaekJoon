e6b5e296298350b9ea19a8316ec0a3621926fa3e
myeongseung <91710678+myeongseung@users.noreply.github.com>
2023-08-02 14:08:49 +0900

    [Bronze III] Title: 공 넣기, Time: 252 ms, Memory: 18672 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] ary = new int[n];
+
+        for(int i=0;i<m;i++){
+            int a =sc.nextInt();
+            int b =sc.nextInt();
+            int c =sc.nextInt();
+            for(int j=a;j<=b;j++){
+                ary[j-1] = c;
+            }
+        }
+
+        for(int i=0;i<ary.length;i++){
+            System.out.print(ary[i] + " ");
+
+        }
+
+
+    }
+}
